dc7cbba0797d1c751802c1a49540d72385c2dfe2
Bradley Shulda <bradleyshulda@Bradleys-MacBook-Pro-2.local>
2026-07-17 20:11:56 -0400

    wire up Apps Script config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.JOB_DASHBOARD_CONFIG = {
-  APPS_SCRIPT_URL: 'REPLACE_ME',
-  TOKEN: 'REPLACE_ME'
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyx4yrxCZ2gEX4bFzMsHYWtE05X_Eoyec-iVIsWtOC9g7aiVEUqr_h_6u_5finAAsk/exec',
+  TOKEN: 'IuacejdZ37x9RBP5nuDMsB8jAsMoJO2y'
 };
